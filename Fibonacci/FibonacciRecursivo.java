d23bdbeddf6d961d38c3822a2ebf1cd1a1922fa6
Henrique Felipe <31190706+HenriqueIni@users.noreply.github.com>
2017-08-21 20:31:15 -0300

    Create FibonacciRecursivo.java

---
+++ Fibonacci/FibonacciRecursivo.java
@@ -0,0 +1,14 @@
+public class FibonacciRecursivo {
+    public static long fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }else if(n == 1){
+            return 1;
+        }else{
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    public static void main(String[] args){
+        System.out.printf("%d", fibonacci(10));
+    }
+}

3ece22ba202c018ff9bc3e99726719dcd6b3f1ca
Henrique Felipe <31190706+HenriqueIni@users.noreply.github.com>
2017-10-23 17:03:43 -0200

    Create SqrPowerMatrix.java

---
+++ 2017/October/MatrixPower/SqrPowerMatrix.java
@@ -0,0 +1,47 @@
+//Code by Henrique Felipe (GitHub: HenriqueIni)
+//The code works only for nonnegative expoent
+public class SqrPowerMatrix {
+    //Computes A to the n. The matrix needs to a square matrix
+    //Exponentiation by squaring
+    public static double[][] powQuad(double A[][], int n) {
+        if (n < 0) throw new IllegalArgumentException("n < 0");
+        if(n == 0) return I(A.length);
+        
+        double[][] p = powQuad(A, n/2);
+        if(n % 2 == 0){
+            return matrixMult(p, p);
+        }else{
+            double[][] p2 = matrixMult(p, p);
+            return matrixMult(A, p2);
+        }        
+    }
+    //Return the identity matrix n x n
+    private static double[][] I(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n <= 0");
+        }
+        double[][] I = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            I[i][i] = 1;
+        }
+        return I;
+    }
+    //Compute A times B
+    private static double[][] matrixMult(double[][] A, double[][] B) {
+        int n = A[0].length;
+        if (n != B.length) {
+            throw new IllegalArgumentException("A.columns != B.rows");
+        }
+        int rows = A.length;
+        int cols = B[0].length;
+        double[][] C = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < n; k++) {
+                    C[i][j] = C[i][j] + A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+}
